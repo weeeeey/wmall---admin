f2049d4d04986de0a32139a042f03e53661ed120
zerojin <qser155@naver.com>
2023-08-19 16:47:29 +0900

    get production 수정/req.url && searchParams

--- app/api/stores/[storeId]/products/route.ts
+++ app/api/stores/[storeId]/products/route.ts
@@ -66,10 +66,29 @@ export async function GET(req: Request, { params }: IParams) {
         if (!params.storeId) {
             return new NextResponse('Store id is required', { status: 400 });
         }
+        const { searchParams } = new URL(req.url);
+        const categoryId = searchParams.get('categoryId') || undefined;
+        const sizeId = searchParams.get('sizeId') || undefined;
+        const colorId = searchParams.get('colorId') || undefined;
+        const isFeatured = searchParams.get('isFeatured');
 
         const products = await client.product.findMany({
             where: {
                 storeId: params.storeId,
+                categoryId,
+                sizeId,
+                colorId,
+                isFeatured: isFeatured ? true : undefined,
+                isArchived: false,
+            },
+            include: {
+                images: true,
+                category: true,
+                color: true,
+                size: true,
+            },
+            orderBy: {
+                createdAt: 'desc',
             },
         });
 
